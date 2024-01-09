0f12c09830629f4782bdf6e09ecbf9d099c1d7f8
Markus Wedler <markuswedler8@gmail.com>
2024-01-09 18:42:26 +1000

    Set up messaging

--- server/app.js
+++ server/app.js
@@ -2,11 +2,18 @@ const WebSocket = require("ws")
 
 const server = new WebSocket.Server({ port: 8080 })
 
-let messages = []
+let messages = [{ sender: "Admin", message: "hello" }]
+let usernames = ["Duck", "Rabbit", "Bee"]
 
 server.on("connection", (socket) => {
-	socket.on("message", (message) => {
-		messages.push(message.toString())
-		console.log("messages:", messages)
+	const usernameIndex = Math.floor(Math.random() * usernames.length)
+	const username = usernames[usernameIndex]
+	socket.send(JSON.stringify({ username, messages }))
+	socket.on("message", (data) => {
+		const parsedData = JSON.parse(data.toString())
+		messages.push(parsedData)
+		server.clients.forEach((client) => {
+			if (client.readyState === WebSocket.OPEN) client.send(JSON.stringify(parsedData))
+		})
 	})
 })
